d6798b3ea54224312054d7dc4d0fcd6d7541fa2c
Jesúspinarte <jesuspinarte@gmail.com>
2023-04-16 11:07:52 -0500

    [POKE-003] - Monster component and selection

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,69 @@
 <script lang="ts">
+  import { goto } from "$app/navigation";
+  import { page } from "$app/stores";
   import type { PageData } from "./$types";
+  import type { IndexMonster } from "./+page";
+  import Monster from "./Monster.svelte";
   import { generations } from "./generations";
 
   export let data: PageData;
+
+  $: monsterId = $page.url.searchParams.get("monsterId") || "";
+  $: monster = data.monsters.find((monster) => monster.id === monsterId);
+
+  $: monsterId2 = $page.url.searchParams.get("monsterId2") || "";
+  $: monster2 = data.monsters.find((monster) => monster.id === monsterId2);
+
+  const updateSearchParams = (key: string, value: string) => {
+    const searchParams = new URLSearchParams($page.url.searchParams);
+    searchParams.set(key, value);
+    goto(`?${searchParams.toString()}`);
+  };
 </script>
 
-{#each data.monsters as monster (monster.id)}
-  <p>{monster.id}: {monster.name}</p>
-{/each}
+{#if monster}
+  <Monster {monster} {updateSearchParams} />
+{/if}
 
-{#each generations as generation (generation.id)}
-  <h1>{generation.name}</h1>
-  <p>Games: {generation.games.join(", ")}</p>
-  <p>Main region: {generation.main_region}</p>
-{/each}
+{#if monster2}
+  <Monster monster={monster2} {updateSearchParams} />
+{/if}
 
-<style></style>
+<div class="generations">
+  {#each generations as generation (generation.id)}
+    <div class="generation">
+      {generation.main_region}
+    </div>
+  {/each}
+</div>
+
+<div class="monsters">
+  {#each data.monsters as monster (monster.id)}
+    <Monster {monster} isInteractive {updateSearchParams} />
+  {/each}
+</div>
+
+<style>
+  .generations {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: center;
+  }
+  .generation {
+    margin: 5px;
+    padding: 5px 10px;
+    border: 1px solid black;
+    background-color: #bbb;
+    color: #222;
+  }
+  .generation:hover {
+    background-color: #eee;
+  }
+  .monsters {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: center;
+  }
+</style>
---
+++ src/routes/Monster.svelte
@@ -0,0 +1,76 @@
+<script lang="ts">
+  import type { IndexMonster } from "./+page";
+
+  export let monster: IndexMonster;
+  export let updateSearchParams: (key: string, value: string) => void;
+  export let isInteractive: boolean = false;
+</script>
+
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<div class={isInteractive ? "monster is-interactive" : "monster"}>
+  <div
+    on:click={() =>
+      isInteractive ? updateSearchParams("monsterId", monster.id) : {}}
+  >
+    <div class="monster-content">
+      <img src={monster.image} alt={monster.name} />
+      {monster.name}
+    </div>
+  </div>
+  <div class="monster-id">
+    {monster.id}
+  </div>
+  {#if isInteractive}
+    <div
+      class="monster-btn-2"
+      on:click={() => updateSearchParams("monsterId2", monster.id)}
+    >
+      Add Monster 2
+    </div>
+  {/if}
+</div>
+
+<style>
+  .monster-id {
+    position: absolute;
+    top: 8px;
+    left: 8px;
+    font-size: 0.8em;
+    color: #aaa;
+    font-family: Arial, Helvetica, sans-serif;
+  }
+  .monster {
+    width: 130px;
+    margin: 10px;
+    padding: 10px;
+    position: relative;
+    background-color: #444;
+    border-radius: 4px;
+  }
+  .is-interactive {
+    cursor: pointer;
+  }
+  .is-interactive:hover {
+    background-color: #777;
+  }
+  .monster-content {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    color: #ddd;
+    font-family: Arial, Helvetica, sans-serif;
+  }
+  .monster-btn-2 {
+    color: #fff;
+    text-align: center;
+    padding: 4px;
+    margin-top: 12px;
+    font-family: Arial, Helvetica, sans-serif;
+  }
+  .monster-btn-2:hover {
+    color: #111;
+    background-color: #eee;
+    opacity: 0.8;
+    border-radius: 4px;
+  }
+</style>
